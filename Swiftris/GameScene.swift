7adb82fe816ca4d010eb8686a796c7cc2850429f
Max Calabrese <max.calabrese@ymail.com>
2014-10-29 12:34:22 +0100

    Add ticks that define that rate that blocks drop Completes A Ticking Clock chapter

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,13 +8,18 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+    var tick:(() -> ())? // A closure
+    var tickLengthMillis = TickLengthLevelOne // the GameScene's current tick length
+    var lastTick:NSDate? //
 
     required init(coder aDecoder: (NSCoder!)){
      fatalError("NSCoder not supported")
     }
     
-    // -> void()?
     override init(size: CGSize) {
         super.init(size: size)
         // 0, 0 in SpriteKit is the bottom-left corner.
@@ -30,5 +35,26 @@ class GameScene: SKScene {
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        if lastTick == nil {
+            return // since we are in a paused state, not reporting elapsed ticks to anyone
+        }
+        // The time passed since the last execution of update.
+        // We multiply the result by -1000 in order to get a positive millisecond value.
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        
+        //  If enough time has elapsed, we must report a tick.
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate() // NSDate.date call in tutorial is unavailble
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
